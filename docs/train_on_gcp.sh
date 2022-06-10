c93c74d27bf118b8f7323ff274d7f9ef5c0a98cf
shink00000 <r82.y252@gmail.com>
2022-06-10 16:48:36 +0900

    [fix]

--- docs/train_on_gcp.sh
+++ docs/train_on_gcp.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-gsutil cp gs://simss-data/voc07+12.zip .
-gsutil cp -r gs://simss-data/assets/* ./assets/
+gsutil cp gs://simdet-data/voc07+12.zip .
+gsutil cp -r gs://simdet-data/assets/* ./assets/
 unzip voc07+12.zip -d ./data
 rm voc07+12.zip
 
 pip install torchmetrics==0.7.3
 pip install tensorboard==2.8.0
+pip install pycocotools==2.0.4
+pip install scipy==1.6.3
 pip install -e .
